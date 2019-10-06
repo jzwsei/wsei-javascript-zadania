c2d2dfb8652b60401e23d986a40be4cd74a66724
Żak Jarosław <jaroslaw.zak@microsoft.wsei.edu.pl>
2019-10-06 10:31:39 +0200

    zad 2.1

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -1,3 +1,4 @@
+// ex. 0
 function distFromAvarage(originalArray) {
     let sum = originalArray.reduce((prev, curr) => {
         return prev + curr;
@@ -12,4 +13,14 @@ function distFromAvarage(originalArray) {
     });
 
     return results;
+}
+
+//ex. 1
+let fruits = ['banan', 'pomarańcza', 'brzoskwinia', 'jabłko'];
+
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
 }
