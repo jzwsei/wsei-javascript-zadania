264bb40d1d5989dce3d58de5fdaa5e4fa5d704a4
Żak Jarosław <jaroslaw.zak@microsoft.wsei.edu.pl>
2019-10-20 10:46:01 +0200

    ex 4-0,1,2,3

---
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -0,0 +1,52 @@
+//ex 0a
+let city = {
+    capital: '',
+    population: 0,
+    president: '',
+    primeMinisters: []
+};
+
+console.log(city);
+
+//ex 0b
+let timeMachine = {
+    shape: '',
+    model: '',
+    run: function (date, place) {
+        return date + ' ' + place;
+    }
+};
+
+console.log(timeMachine);
+timeMachine.run('2019-10-19', 'London');
+
+//ex 1
+let book = {
+    title: 'Narnia',
+    author: 'CS Lewis', 
+    numberOfPages: 100
+};
+
+console.log(book);
+
+//ex 2
+let person = {
+    name: 'John',
+    age: 15,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
+
+console.log(person.name, person.age);
+person.sayHello();
+
+//ex 3
+let recipe = {
+    title: 'Naleśniki',
+    servings: 150
+};
+
+recipe.ingredients = ['mąka', 'woda', 'cukier'];
+
+console.log(recipe);
