70ca297ed51c8e19f3c5f530580e0a88765b71f8
Żak Jarosław <jaroslaw.zak@microsoft.wsei.edu.pl>
2019-10-20 10:24:31 +0200

    ex 3.0

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -13,3 +13,21 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+//ex. 0
+function checkArray(inArrays) {
+    let result = [], i;
+
+    inArrays.forEach(numbers => {
+        for (i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 !== 0) {
+                result.push(false);
+                return;
+            }
+        }
+
+        result.push(true);
+    });
+
+    return result;
+}
