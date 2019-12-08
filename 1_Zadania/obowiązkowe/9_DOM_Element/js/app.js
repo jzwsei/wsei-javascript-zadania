14f93e3563f95ffc1cb62d2ea9ba531ab6cce1f6
Żak Jarosław <jaroslaw.zak@microsoft.wsei.edu.pl>
2019-12-08 10:12:37 +0100

    ex 9.2-6

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -36,4 +36,56 @@ document.addEventListener("DOMContentLoaded", function () {
              console.log(collectionItem.tagName, collectionItem.className);
          }
      }
+
+     // ex 2
+     for (collectionItem of blocks) {
+         // zawiera html wewnątrz tagu elementu-rodzica
+         console.log(collectionItem.innerHTML);
+         // zawiera html wraz z tagiem elementu-rodzica
+         console.log(collectionItem.outerHTML);         
+     }
+
+    // ex 3
+    let myElement = window.document.getElementById('mainFooter');
+
+    function getId(element) {
+        return element.id;
+    }
+
+    console.log(getId(myElement));
+
+    // ex 4
+    function getTags(elementsArray) {
+        let tags = []; 
+
+        for (let child of elementsArray) {
+            tags.push(child.tagName);
+        }
+
+        return tags;
+    }
+
+    console.log(getTags(childElements));
+
+    // ex 5
+    function getClassInfo(element) {
+        return element.className;
+    }
+
+    console.log(getClassInfo(banner));
+
+    // ex 6
+    let foundElements = window.document.querySelectorAll('nav li');
+
+    function setDataDirection(elements) {
+        for (let item of elements) {
+            if (item.getAttribute('data-direction')) {
+                continue;
+            }
+
+            item.setAttribute('data-direction', 'top');
+        }
+    }
+
+    setDataDirection(foundElements);
 });
