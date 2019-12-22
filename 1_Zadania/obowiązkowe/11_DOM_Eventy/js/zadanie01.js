774b7f0f6f6a56d1e0900c0d932ce0dfa21a4624
Żak Jarosław <jaroslaw.zak@microsoft.wsei.edu.pl>
2019-12-22 10:26:49 +0100

    ex 11.1

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
@@ -1,5 +1,9 @@
-var menu = document.querySelector("#menu");
-var paragraf = document.querySelector("p");
+// po przesunięciu script do head, kod wykonuje się zanim dokument html zostanie załadowany, przez co występują błędy - nie można operować na elementach które nie istnieją
+// po dodaniu obsługi eventu DOMContentLoaded JS zawsze (niezależnie od położenia tagu script) czeka na załadowanie całego drzewa dom, po tym dopiero rozpoczyna wykonywanie kodu
+window.document.addEventListener('DOMContentLoaded', function() {
+    var menu = document.querySelector("#menu");
+    var paragraf = document.querySelector("p");
 
-menu.classList.add("menu");
-paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+    menu.classList.add("menu");
+    paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+});
