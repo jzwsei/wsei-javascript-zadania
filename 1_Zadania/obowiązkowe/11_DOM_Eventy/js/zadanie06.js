e80ba5ba6dd7e93198239ccb308cf93dd8d43414
Żak Jarosław <jaroslaw.zak@microsoft.wsei.edu.pl>
2019-12-22 11:05:45 +0100

    ex 11.06

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie06.js
@@ -0,0 +1,17 @@
+window.document.addEventListener('DOMContentLoaded', function() {
+    let globalXEl = document.querySelector('#globalX'),
+        globalYEl = document.querySelector('#globalY'),
+        localXEl = document.querySelector('#localX'),
+        localYEl = document.querySelector('#localY'),
+        divEl = document.querySelector('div');
+
+    divEl.addEventListener('mousemove', ev => {
+        localXEl.innerHTML = ev.clientX;
+        localYEl.innerHTML = ev.clientY;
+    });
+
+    window.addEventListener('mousemove', ev => {
+        globalXEl.innerHTML = ev.clientX;
+        globalYEl.innerHTML = ev.clientY;
+    });
+});
